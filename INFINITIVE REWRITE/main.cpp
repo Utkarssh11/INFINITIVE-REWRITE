5c4ba3f70f6153887b6c031005fe3afadfc8325b
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-04-14 12:38:20 +0530

    Add files via upload

---
+++ INFINITIVE REWRITE/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int i=0;
+cout<<"your string is ready....... "<<endl;
+char s1[500];
+cin.getline(s1,500);
+while(1!=0){
+i++;
+cout<<s1<<"      "<<i<<endl;
+}}
